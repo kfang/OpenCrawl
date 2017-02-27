c4980432e243994eecd8664899807832fbc251a7
kevin <hkr.fang@gmail.com>
2017-02-27 13:35:22 -0800

    send back the error in the response

--- src/main/scala/com/github/kfang/opencrawl/services/JsoupService.scala
+++ src/main/scala/com/github/kfang/opencrawl/services/JsoupService.scala
@@ -9,37 +9,44 @@ import scala.util.{Failure, Success, Try}
 class JsoupService extends Actor with ActorLogging {
 
   def process(job: JsoupJob, requester: ActorRef): Unit = {
-    val document = Jsoup.connect(job.url).get()
-    val title = document.title()
+    Try {
+      val document = Jsoup.connect(job.url).get()
+      val title = document.title()
 
-    val results = job.selectors.map(sel => {
+      val results = job.selectors.map(sel => {
 
-      val selection = document.select(sel.selector)
+        val selection = document.select(sel.selector)
 
-      val extractor = sel.extractor.extractor
-      val extractorArgs = sel.extractor.args.getOrElse(Nil)
-      val key = sel.key
+        val extractor = sel.extractor.extractor
+        val extractorArgs = sel.extractor.args.getOrElse(Nil)
+        val key = sel.key
 
-      Try(extractor match {
-        case "attr" =>
-          extractorArgs match {
-            case a1 :: _ => selection.attr(a1)
-            case Nil     => selection.attr("")
-          }
-        case "text" => selection.text()
-        case "html" => selection.html()
-      }) match {
-        case Success(value) =>
-          JsoupJobSelectorResult(key = key, value = Some(value), error = None, numElementsSelected = selection.size())
-        case Failure(error) =>
-          JsoupJobSelectorResult(key = key, value = None, error = Some(error.getMessage), numElementsSelected = selection.size())
-      }
-    })
+        Try(extractor match {
+          case "attr" =>
+            extractorArgs match {
+              case a1 :: _ => selection.attr(a1)
+              case Nil => selection.attr("")
+            }
+          case "text" => selection.text()
+          case "html" => selection.html()
+        }) match {
+          case Success(value) =>
+            JsoupJobSelectorResult(key = key, value = Some(value), error = None, numElementsSelected = selection.size())
+          case Failure(error) =>
+            JsoupJobSelectorResult(key = key, value = None, error = Some(error.getMessage), numElementsSelected = selection.size())
+        }
+      })
 
-    requester ! JsoupJobResult(
-      pageTitle = title,
-      results = results
-    )
+      JsoupJobResult(
+        pageTitle = title,
+        results = results
+      )
+
+    } match {
+      case Success(res) => requester ! res
+      case Failure(err) => requester ! JsoupJobResult(error = Some(err.getMessage))
+    }
+
   }
 
   def receive: Receive = {
